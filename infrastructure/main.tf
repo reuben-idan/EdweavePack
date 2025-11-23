a6461c11fb427ab83e15daafab8c814ac65120ea
Cascade <cascade@example.com>
2025-11-23 23:04:08 +0000

    Fix PostgreSQL version for eu-north-1

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -128,7 +128,7 @@ resource "aws_security_group" "rds" {
 resource "aws_db_instance" "main" {
   identifier     = "${var.project_name}-db"
   engine         = "postgres"
-  engine_version = "15.4"
+  engine_version = "15.8"
   instance_class = "db.t3.micro"
   
   allocated_storage     = 20
